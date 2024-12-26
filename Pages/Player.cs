657fc9b6885d9102f5091460b4a7ea97c9155492
jokerclay <2079209547@qq.com>
2024-12-26 11:59:16 +0800

    added example

---
+++ Pages/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace ComponentCommunication.Pages;
+
+public class Player
+{
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public bool IsActive { get; set; }
+}
+    
